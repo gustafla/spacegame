ef4e185ba34e960f7412f4880db30ff1ec64a59e
Lauri Gustafsson <luutifa@gmail.com>
2017-11-30 02:50:44 +0200

    Test rendering

--- game.c
+++ game.c
@@ -1,14 +1,22 @@
 #include "game.h"
 
 int update_game(GameState *state) {
+  /* Poll events and return stop if SDL_QUIT */
   SDL_PollEvent(&state->event);
   if (state->event.type == SDL_QUIT) {
     return 0;
   }
+
+
+
   return 1;
 }
 
 void draw_game(GameState *state, SDL_Renderer *renderer) {
+  SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+  SDL_RenderDrawLine(renderer, 100, 100, 200, 200);
 
+  /* Leave a BG color for next clear */
+  SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
 }
 
